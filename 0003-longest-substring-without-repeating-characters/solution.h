247ac2a96619a5f877bac65c5dc62e54d2023a6d
alexkingcodes <thealexking7@gmail.com>
2025-10-20 17:02:34 -0500

    chore(longest-substring-without-repeating-characters): add solution.h

---
+++ 0003-longest-substring-without-repeating-characters/solution.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// 0003. Longest Substring Without Repeating Characters
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s);
+};
