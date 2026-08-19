d2d7ba414433b254c2090ac7bca134accb36cc6a
alexkingcodes <thealexking7@gmail.com>
2026-08-19 16:30:02 -0500

    chore(longest-palindromic-substring): add solution.cpp

---
+++ 0005-longest-palindromic-substring/solution.cpp
@@ -0,0 +1,9 @@
+#include "solution.h"
+
+// 0005. Longest Palindromic Substring
+// https://leetcode.com/problems/longest-palindromic-substring/
+
+string Solution::longestPalindrome(string s) {
+    // TODO: implement
+}
+
