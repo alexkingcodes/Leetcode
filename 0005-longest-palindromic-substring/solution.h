5a6ec23cb963cd5696d82f624c85283cebd4c02c
alexkingcodes <thealexking7@gmail.com>
2026-08-19 16:30:01 -0500

    chore(longest-palindromic-substring): add solution.h

---
+++ 0005-longest-palindromic-substring/solution.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// 0005. Longest Palindromic Substring
+// https://leetcode.com/problems/longest-palindromic-substring/
+
+class Solution {
+public:
+    string longestPalindrome(string s);
+};
