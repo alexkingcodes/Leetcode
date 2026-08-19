fb4c384162942e5da3a648471fefd7c5adfaa6ab
alexkingcodes <thealexking7@gmail.com>
2026-08-19 16:32:02 -0500

    chore(regular-expression-matching): add solution.h

---
+++ 0010-regular-expression-matching/solution.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// 0010. Regular Expression Matching
+// https://leetcode.com/problems/regular-expression-matching/
+
+class Solution {
+public:
+    bool isMatch(string s, string p);
+};
