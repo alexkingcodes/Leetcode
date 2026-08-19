634da0a91d873b44ce401f1d143be652ca037842
alexkingcodes <thealexking7@gmail.com>
2026-08-19 16:30:52 -0500

    chore(reverse-integer): add solution.h

---
+++ 0007-reverse-integer/solution.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// 0007. Reverse Integer
+// https://leetcode.com/problems/reverse-integer/
+
+class Solution {
+public:
+    int reverse(int x);
+};
