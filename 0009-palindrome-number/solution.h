d04d8bc66b73cbd9d4935d8fe0acc689e985ab34
alexkingcodes <thealexking7@gmail.com>
2026-08-19 16:31:18 -0500

    chore(palindrome-number): add solution.h

---
+++ 0009-palindrome-number/solution.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// 0009. Palindrome Number
+// https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+public:
+    bool isPalindrome(int x);
+};
