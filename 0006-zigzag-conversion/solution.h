0f1b04caf1b07321c5ddec501db739752c968d92
alexkingcodes <thealexking7@gmail.com>
2026-08-19 16:30:16 -0500

    chore(zigzag-conversion): add solution.h

---
+++ 0006-zigzag-conversion/solution.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// 0006. Zigzag Conversion
+// https://leetcode.com/problems/zigzag-conversion/
+
+class Solution {
+public:
+    string convert(string s, int numRows);
+};
