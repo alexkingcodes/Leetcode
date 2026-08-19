cf82db93301e7e2c4e7b06429a2d29e5861f5671
alexkingcodes <thealexking7@gmail.com>
2026-08-19 16:30:51 -0500

    chore(reverse-integer): add main.cpp

---
+++ 0007-reverse-integer/main.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    // TODO: read input and call Solution methods
+    Solution sol;
+    (void)sol;
+
+    return 0;
+}
