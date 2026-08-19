0116ab1a5b41c94e038faace38d813621232a454
alexkingcodes <thealexking7@gmail.com>
2026-08-19 16:31:04 -0500

    chore(string-to-integer-atoi): add solution.h

---
+++ 0008-string-to-integer-atoi/solution.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// 0008. String to Integer (atoi)
+// https://leetcode.com/problems/string-to-integer-atoi/
+
+class Solution {
+public:
+    int myAtoi(string s);
+};
